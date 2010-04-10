17ada981832e39114c96004ad42befbf1338753f
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-04-10 13:56:39 +0000

    Simple Log method for our Plugin

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/RefactoringPlugin.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/RefactoringPlugin.java
@@ -1,5 +1,7 @@
 package tinyos.yeti.refactoring;
 
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -11,9 +13,13 @@ public class RefactoringPlugin extends AbstractUIPlugin {
 	// The plug-in ID
 	public static final String PLUGIN_ID = "TinyOS_Refactoring";
 
+	public enum LogLevel {
+		CANCEL, ERROR, INFO, OK, WARNING
+	}
+
 	// The shared instance
 	private static RefactoringPlugin plugin;
-	
+
 	/**
 	 * The constructor
 	 */
@@ -22,7 +28,10 @@ public class RefactoringPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
@@ -31,16 +40,50 @@ public class RefactoringPlugin extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 * 
+	 * @see
+	 * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext
+	 * )
 	 */
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
 		super.stop(context);
 	}
 
+	public void log(LogLevel level, String message) {
+		int severity;
+		switch (level) {
+		case CANCEL:
+			severity = IStatus.CANCEL;
+			break;
+
+		case ERROR:
+			severity = IStatus.ERROR;
+			break;
+
+		case INFO:
+			severity = IStatus.INFO;
+			break;
+
+		case OK:
+			severity = IStatus.OK;
+			break;
+
+		case WARNING:
+			severity = IStatus.WARNING;
+			break;
+
+		default:
+			log(LogLevel.ERROR, message);
+			throw new IllegalArgumentException("No Rule for LogLevel \""
+					+ level + "\". Message loged as ERROR.");
+		}
+		this.getLog().log(new Status(severity, PLUGIN_ID, message));
+	}
+
 	/**
 	 * Returns the shared instance
-	 *
+	 * 
 	 * @return the shared instance
 	 */
 	public static RefactoringPlugin getDefault() {
