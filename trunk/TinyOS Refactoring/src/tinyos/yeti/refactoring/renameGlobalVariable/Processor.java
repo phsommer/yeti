c44e22297831dd5c26f89c06679d9b2736401ce6
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-07-05 09:21:42 +0000

    Code to Rename a global variable should now work, but is not tested. This Commit is done to be able to test the Code together. (Not ment to be used)

--- trunk/TinyOS Refactoring/src/tinyos/yeti/refactoring/renameGlobalVariable/Processor.java
+++ trunk/TinyOS Refactoring/src/tinyos/yeti/refactoring/renameGlobalVariable/Processor.java
@@ -1,14 +1,21 @@
 package tinyos.yeti.refactoring.renameGlobalVariable;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.LinkedList;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
+import org.eclipse.ltk.core.refactoring.NullChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
 import org.eclipse.ltk.core.refactoring.TextChange;
 import org.eclipse.ltk.core.refactoring.TextFileChange;
@@ -18,9 +25,24 @@ import org.eclipse.ltk.core.refactoring.participants.SharableParticipants;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.ReplaceEdit;
 
+import tinyos.yeti.TinyOSPlugin;
+import tinyos.yeti.builder.ProjectResourceCollector;
+import tinyos.yeti.ep.IParseFile;
+import tinyos.yeti.ep.parser.INesCParser;
+import tinyos.yeti.ep.parser.reference.IASTReference;
+import tinyos.yeti.ep.parser.standard.ASTModelPath;
+import tinyos.yeti.model.ProjectModel;
+import tinyos.yeti.nature.MissingNatureException;
+import tinyos.yeti.nesc.FileMultiReader;
+import tinyos.yeti.nesc12.ep.NesC12AST;
 import tinyos.yeti.nesc12.parser.ast.nodes.ASTNode;
+import tinyos.yeti.nesc12.parser.ast.nodes.declaration.DeclaratorName;
+import tinyos.yeti.nesc12.parser.ast.nodes.declaration.InitDeclarator;
+import tinyos.yeti.nesc12.parser.ast.nodes.expression.IdentifierExpression;
 import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
-import tinyos.yeti.refactoring.ActionHandlerUtil;
+import tinyos.yeti.refactoring.ASTUtil;
+import tinyos.yeti.refactoring.RefactoringPlugin;
+import tinyos.yeti.refactoring.RefactoringPlugin.LogLevel;
 import tinyos.yeti.refactoring.rename.RenameProcessor;
 
 public class Processor extends RenameProcessor {
@@ -52,47 +74,193 @@ public class Processor extends RenameProcessor {
 		if (!isApplicable()) {
 			ret.addFatalError("The Refactoring is no Accessable");
 		}
+		if (!isGlobalVariable(getSelectedIdentifier())) {
+			ret.addFatalError("No global variable selected.");
+		}
 		return ret;
-		// TODO checkInitialContions not yet implemented
+	}
+
+	private NesC12AST getAst(IFile iFile, IProgressMonitor monitor)
+			throws IOException, MissingNatureException {
+		// Create Parser for File to construct an AST
+		IProject project = info.getEditor().getProject();
+		ProjectModel projectModel = TinyOSPlugin.getDefault().getProjectTOS(
+				project).getModel();
+
+		File file = iFile.getLocation().toFile();
+		IParseFile parseFile = projectModel.parseFile(file);
+
+		INesCParser parser = projectModel.newParser(parseFile, null, monitor);
+		parser.setCreateAST(true);
+		parser.parse(new FileMultiReader(file), monitor);
+
+		return (NesC12AST) parser.getAST();
+	}
+
+	private boolean isGlobalVariable(Identifier variable) {
+		// Local Variable are of corse no globle variables
+		if (getVarUtil().isLocalVariable(variable)) {
+			return false;
+		}
+
+		// Is true in case a variable is used.
+		IdentifierExpression ie = (IdentifierExpression) ASTUtil
+				.getParentForName(variable, IdentifierExpression.class);
+		if (ie != null) {
+			return true;
+		}
+
+		// Is true in case a variable is defined.
+		DeclaratorName dn = (DeclaratorName) ASTUtil.getParentForName(variable,
+				DeclaratorName.class);
+		if (dn != null) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean containsOccurenceOfVariable(IFile file,
+			Identifier variable, IProgressMonitor monitor)
+			throws MissingNatureException {
+		ASTModelPath fPath = getPathOfVariable(variable);
+		ProjectModel model = TinyOSPlugin.getDefault().getProjectTOS(
+				info.getEditor().getProject()).getModel();
+		IASTReference[] referenceArray = model.getReferences(model
+				.parseFile(file), monitor);
+		for (IASTReference ref : referenceArray) {
+			if (ref.getTarget().equals(fPath)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the ASTModelPath of the variable or null if the Identifier does
+	 * not represent a Variable or the Variable is unknown
+	 * 
+	 * @param variable
+	 * @return
+	 */
+	private ASTModelPath getPathOfVariable(Identifier variable) {
+		IdentifierExpression ie = (IdentifierExpression) ASTUtil
+				.getParentForName(variable, IdentifierExpression.class);
+		if (ie != null) {
+			// this might return null if the node is unknown
+			return ie.resolveField().getPath();
+		}
+
+		InitDeclarator id = (InitDeclarator) ASTUtil.getParentForName(variable,
+				InitDeclarator.class);
+		if (id != null) {
+			// this might return null if the node is unknown
+			return id.resolveField().getPath();
+		}
+
+		return null;
+	}
+
+	private Collection<IFile> getFilesContainingVariable(Identifier variable,
+			IProgressMonitor pm) throws CoreException {
+		IProject project = info.getEditor().getProject();
+		ProjectResourceCollector collector = new ProjectResourceCollector();
+		try {
+			TinyOSPlugin.getDefault().getProjectTOS(project).acceptSourceFiles(
+					collector);
+		} catch (MissingNatureException e) {
+			RefactoringPlugin.getDefault().log(
+					LogLevel.WARNING,
+					"Refactroing was called while Plugin was not ready: "
+							+ e.getMessage());
+			throw new CoreException(new Status(IStatus.ERROR,
+					RefactoringPlugin.PLUGIN_ID,
+					"Plugin wasn't ready while calling Rename global Variable Refactoring: "
+							+ e.getMessage()));
+		}
+
+		LinkedList<IFile> files = new LinkedList<IFile>();
+		for (IResource resource : collector.resources) {
+			if (resource.getType() == IResource.FILE) {
+				IFile file = (IFile) resource;
+				try {
+					if (containsOccurenceOfVariable(file, variable, pm)) {
+						files.add((IFile) resource);
+					}
+				} catch (MissingNatureException e) {
+					throw new CoreException(new Status(IStatus.ERROR,
+							RefactoringPlugin.PLUGIN_ID,
+							"Refactoring was called while Plagin was not ready: "
+									+ e.getMessage()));
+				}
+			}
+		}
+		return files;
+	}
+
+	private Change renameAllOccurencesInFile(IFile file,IProgressMonitor pm) {
+		String changeName = "Replacing Variable " + info.getOldName()
+				+ " with " + info.getNewName() + " in Document " + file;
+
+		System.err.println(changeName);
+
+		MultiTextEdit multiTextEdit = new MultiTextEdit();
+		TextChange renameAllOccurencesInFile = new TextFileChange(changeName,
+				file);
+		renameAllOccurencesInFile.setEdit(multiTextEdit);
+		
+
+		// BEGIN: getting AST and handling Errors
+		NesC12AST ast = null;
+		try {
+			ast = getAst(file, pm);
+		} catch (IOException e) {
+			RefactoringPlugin.getDefault().log(
+					LogLevel.WARNING,
+					e.getClass().getCanonicalName() + " while parsing File: "
+							+ file.getFullPath().toOSString());
+		} catch (MissingNatureException e) {
+			RefactoringPlugin.getDefault().log(
+					LogLevel.WARNING,
+					e.getClass().getCanonicalName() + " while parsing File: "
+							+ file.getFullPath().toOSString());
+		} finally {
+			if (ast == null) {
+				return new NullChange();
+			}
+		}
+		ASTNode astRoot = ast.getRoot();
+		// END: getting AST and handling Errors
+		
+		Collection<Identifier> occurences = getVarUtil().getAllIdentifiers(
+				astRoot, info.getOldName());
+
+		// changing the name of all occurrences in the file
+		for (Identifier occurece : occurences) {
+
+			int beginOffset = getAstUtil().start(occurece);
+			int endOffset = getAstUtil().end(occurece);
+			int length = endOffset - beginOffset;
+			multiTextEdit.addChild(new ReplaceEdit(beginOffset, length, info
+					.getNewName()));
+		}
+		return null;
 	}
 
 	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException,
 			OperationCanceledException {
 
+		Identifier selectedVariable = getSelectedIdentifier();
+
 		CompositeChange ret = new CompositeChange("Rename Global Variable "
 				+ info.getOldName() + " to " + info.getNewName());
 
-		// TODO: Find all Files where the variable occures (4 now just the
-		// editors file is used)
-		Collection<IFile> files = new LinkedList<IFile>();
-		files.add(ActionHandlerUtil.getInputFile(info.getEditor()));
+		Collection<IFile> files = getFilesContainingVariable(selectedVariable,
+				pm);
 
 		for (IFile file : files) {
-			String changeName = "Replacing Variable " + info.getOldName()
-					+ " with " + info.getNewName() + " in Document " + file;
-			System.err.println(changeName);
-			MultiTextEdit multiTextEdit = new MultiTextEdit();
-			TextChange renameAllOccurencesInFile = new TextFileChange(
-					changeName, file);
-			renameAllOccurencesInFile.setEdit(multiTextEdit);
-			ret.add(renameAllOccurencesInFile);
-
-			// TODO: Get an AST of the File (4 now, just the ast of the open
-			// file is used.)
-			ASTNode astRoot = getAstUtil().getAST().getRoot();
-
-			Collection<Identifier> occurences = getVarUtil().getAllIdentifiers(
-					astRoot, info.getOldName());
-			System.err.println("huj tja");
-			for (Identifier occurece : occurences) {
-				int beginOffset = getAstUtil().start(occurece);
-				int endOffset = getAstUtil().end(occurece);
-				int length = endOffset - beginOffset;
-				multiTextEdit.addChild(new ReplaceEdit(beginOffset, length,
-						info.getNewName()));
-			}
-
+			ret.add(renameAllOccurencesInFile(file,pm));
 		}
 
 		return ret;
