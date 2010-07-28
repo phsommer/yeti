1072043452365a85a8a2c7c1965a471107bb651b
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-07-28 13:07:04 +0000

    Extract function is able to find out what Variable need to be passed as Input and Output parameters. But dosen't do anything to the code yet.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/InputPage.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/InputPage.java
@@ -1,18 +1,32 @@
 package tinyos.yeti.refactoring.extractFunction;
 
+import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 
 public class InputPage extends UserInputWizardPage {
 
+	Info info;
+	
 	public InputPage(Info info) {
 		super(info.getInputPageName());
-		// TODO Auto-generated constructor stub
+		this.info = info;
 	}
 
 	@Override
 	public void createControl(Composite parent) {
-		// TODO Auto-generated method stub
+		Composite root = new Composite(parent, SWT.NONE);
+
+		GridLayout layout = new GridLayout(1, false);
+		root.setLayout(layout);
+
+		setControl(root);
+		initializeDialogUnits(root);
+		Dialog.applyDialogFont(root);
+		
+
 		
 	}
 
