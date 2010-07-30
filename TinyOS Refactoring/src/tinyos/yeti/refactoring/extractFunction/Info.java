e10a04e9019bbef3c97750a9620defc60c64d79b
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-07-30 12:08:11 +0000

    Moved getStatementsToExtract form the Processor in here. getSelectionBegin & End return now the Begin and End pos of the selected Statements. For the real Selection, getOrigSelectionBegin & End got introduced.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/Info.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/Info.java
@@ -1,7 +1,16 @@
 package tinyos.yeti.refactoring.extractFunction;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import tinyos.yeti.editors.NesCEditor;
+import tinyos.yeti.nesc12.parser.ast.nodes.ASTNode;
+import tinyos.yeti.nesc12.parser.ast.nodes.statement.CompoundStatement;
+import tinyos.yeti.nesc12.parser.ast.nodes.statement.Statement;
 import tinyos.yeti.refactoring.RefactoringInfo;
+import tinyos.yeti.refactoring.ast.ASTPositioning;
+import tinyos.yeti.refactoring.utilities.ASTUtil;
+import tinyos.yeti.refactoring.utilities.ASTUtil4Functions;
 
 public class Info extends RefactoringInfo{
 
@@ -20,12 +29,62 @@ public class Info extends RefactoringInfo{
 		return functionName;
 	}
 	
+	/**
+	 * Returns the Statements that are selected to be extracted.
+	 * It is guaranteed that at least one Statement is selected. Otherwise
+	 * @throws IllegalStateException If no statement is selected to be extracted.
+	 */
+	public List<Statement> getStatementsToExtract() {
+		int begin = getOrigSelectionBegin();
+		int end = getOrigSelectionEnd();
+		ASTPositioning util = getAstPositioning();
+		LinkedList<Statement> ret = new LinkedList<Statement>();
+
+		CompoundStatement compSt = getAstPositioning().getDeepedstSuperCompoundSuperstatement(begin);
+
+		for (int i = 0; i < compSt.getChildrenCount()
+				&& util.end(compSt.getChild(i)) <= end; i++) {
+			ASTNode statement = compSt.getChild(i);
+			if (util.start(statement) >= begin) {
+				if (!(statement instanceof Statement)) {
+					throw new RuntimeException(
+							"Found a non Statement as Child of a Compound Statement. It was a "
+									+ statement.getClass().getCanonicalName());
+				}
+				ret.add((Statement) statement);
+			}
+		}
+		if(ret.size() < 1){
+			throw new IllegalStateException("At least one Statement has to be Selected.");
+		}
+
+		return ret;
+	}
+	
+	/**
+	 * Give the Position just before the fist selected Statement.
+	 */
 	public int getSelectionBegin(){
+		LinkedList<Statement> statements = new LinkedList<Statement>(getStatementsToExtract());
+		return getAstPositioning().start(statements.getFirst());
+	}
+	
+	/**
+	 * Give the Position just after the Last selected Statement.
+	 */
+	public int getSelectionEnd(){
+		LinkedList<Statement> statements = new LinkedList<Statement>(getStatementsToExtract());
+		return getAstPositioning().end(statements.getLast());
+	}
+	
+	
+	public int getOrigSelectionBegin(){
 		return getSelection().getOffset();
 	}
 	
-	public int getSelectionEnd(){
-		return getSelectionBegin()+getSelection().getLength();
+	
+	public int getOrigSelectionEnd(){
+		return getOrigSelectionBegin()+getSelection().getLength();
 	}
 
 }
