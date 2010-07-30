0c18d5c79cd94a8e9912031ff08e43393b278e65
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-07-30 15:29:40 +0000

    availability Check does now call checkInitialConditions

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/AvailabilityTester.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/AvailabilityTester.java
@@ -1,15 +1,30 @@
 package tinyos.yeti.refactoring.extractFunction;
 
+
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.jface.text.ITextSelection;
 
+import tinyos.yeti.editors.NesCEditor;
 import tinyos.yeti.refactoring.AvailabilityTester.IRefactoringAvailabilityTester;
+import tinyos.yeti.refactoring.utilities.ActionHandlerUtil;
 
 public class AvailabilityTester implements IRefactoringAvailabilityTester{
 
 	@Override
 	public boolean test(ITextSelection receiver) {
-		// TODO
-		return true;
+		NesCEditor editor = ActionHandlerUtil.getActiveEditor().getNesCEditor();
+		Info info = new Info(editor);
+		Processor processor = new Processor(info);
+
+		try {
+			return processor.checkInitialConditions(null).isOK();
+		} catch (OperationCanceledException e) {
+			return false;
+		} catch (CoreException e) {
+			return false;
+		}
 	}
 
 }
