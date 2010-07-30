d735c5c4d2413e4d56daa9e7a3147c91ac48fb08
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-07-30 15:05:08 +0000

    The function Name can now be specefied

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/InputPage.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/extractFunction/InputPage.java
@@ -3,8 +3,13 @@ package tinyos.yeti.refactoring.extractFunction;
 import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.ltk.ui.refactoring.UserInputWizardPage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 public class InputPage extends UserInputWizardPage {
 
@@ -25,9 +30,19 @@ public class InputPage extends UserInputWizardPage {
 		setControl(root);
 		initializeDialogUnits(root);
 		Dialog.applyDialogFont(root);
+		Label lblNewName = new Label(root, SWT.NONE);
+		lblNewName.setText("Function Name:");
+		lblNewName.forceFocus();
 		
-
+		final Text newNameTextField = new Text(root, SWT.NONE);
 		
+	    newNameTextField.setLayoutData( new GridData( GridData.FILL_HORIZONTAL ) );
+	    newNameTextField.selectAll();
+	    newNameTextField.addKeyListener( new KeyAdapter() {
+	      public void keyReleased( final KeyEvent e ) {
+	        info.setFunctionName( newNameTextField.getText() );
+	      }
+	    } );		
 	}
 
 }
