0b141fbe8aa32e07baf3bec57354194430a45072
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-08-07 09:55:40 +0000

    Refactored local variable renaming.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/rename/local/variable/RenameLocalVariableProcessor.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/rename/local/variable/RenameLocalVariableProcessor.java
@@ -1,7 +1,8 @@
 package tinyos.yeti.refactoring.rename.local.variable;
 
 import java.util.Collection;
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
@@ -11,10 +12,6 @@ import org.eclipse.ltk.core.refactoring.Change;
 import org.eclipse.ltk.core.refactoring.CompositeChange;
 import org.eclipse.ltk.core.refactoring.NullChange;
 import org.eclipse.ltk.core.refactoring.RefactoringStatus;
-import org.eclipse.ltk.core.refactoring.TextChange;
-import org.eclipse.ltk.core.refactoring.TextFileChange;
-import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
-import org.eclipse.text.edits.MultiTextEdit;
 
 import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
 import tinyos.yeti.nesc12.parser.ast.nodes.statement.CompoundStatement;
@@ -29,96 +26,47 @@ public class RenameLocalVariableProcessor extends RenameProcessor {
 
 	private RenameInfo info;
 
+	private IFile containingFile;
+	private CompoundStatement declaringCompound;
+	private Collection<Identifier> affectedIdentifiers;
+
 	public RenameLocalVariableProcessor(RenameInfo info) {
 		super(info);
 		this.info = info;
 	}
 
 	@Override
-	public RefactoringStatus checkFinalConditions(IProgressMonitor pm,
-			CheckConditionsContext context) throws CoreException,
-			OperationCanceledException {
-		RefactoringStatus ret = new RefactoringStatus();
-		if (info.getNewName() == null) {
-			ret.addFatalError("Please enter a new Name for the Variabel.");
-		}
-
-		return ret;
-	}
-
-	@Override
-	public RefactoringStatus checkInitialConditions(IProgressMonitor pm)
-			throws CoreException, OperationCanceledException {
-		RefactoringStatus ret = new RefactoringStatus();
-		if (!isApplicable()) {
-			ret.addFatalError("A local Variable must be selected.");
-		}
-		IFile sourceFile = ActionHandlerUtil.getInputFile(info.getEditor());
-		if (sourceFile == null || !sourceFile.exists()) {
-			ret.addFatalError("The File you want wo Refactor, does not exist.");
-		} else if (sourceFile.isReadOnly()) {
-			ret.addFatalError("The File you want to Refactor is read only.");
-		}
-		return ret;
-	}
-
-	@Override
-	public boolean isApplicable() throws CoreException {
-		// Tests if a LOCAL Variable is selected
-		Identifier identifier = getSelectedIdentifier();
-		if (identifier == null)
-			return false;
-		CompoundStatement compound = astUtil4Variables.findDeclaringCompoundStatement(identifier);
-		return compound != null;
-	}
-
-	@Override
 	public Change createChange(IProgressMonitor pm) throws CoreException,
 			OperationCanceledException {
-
-		IFile inputFile = ActionHandlerUtil.getInputFile(info.getEditor());
-		// Create The Changes
-		MultiTextEdit multiTextEdit = new MultiTextEdit();
-		String changeName = "Replacing Variable " + info.getOldName()+ " with " + info.getNewName() + " in Document " + inputFile;
-		TextChange renameAllOccurences = new TextFileChange(changeName,
-				inputFile);
-		// IDocument document=info.getEditor().getDocument();
-		// TextChange renameOneOccurence = new
-		// DocumentChange(changeName,document);
-		renameAllOccurences.setEdit(multiTextEdit);
-		CompositeChange ret = new CompositeChange("Rename Local Variable "
-				+ info.getOldName() + " to " + info.getNewName());
-		ret.add(renameAllOccurences);
-		Collection<Identifier> identifiers = this.selectedIdentifiersIfLocal();
-		if (identifiers.size() == 0) {
-			return new NullChange();
+		
+		CompositeChange ret = new CompositeChange("Rename Local Variable "+ info.getOldName() + " to " + info.getNewName());
+		Map<IFile,Collection<Identifier>> map=new HashMap<IFile, Collection<Identifier>>();
+		map.put(containingFile, affectedIdentifiers);
+		try {
+			super.addChanges("local variable", map, ret, pm);
+		} catch (Exception e){
+			ret.add(new NullChange("Exception occured during change creation. See project log for more information."));
+			getProjectUtil().log("Exception occured during change creation.",e);
 		}
-		addChanges4Identifiers(identifiers,info.getNewName(),multiTextEdit,null);
 		return ret;
 	}
 
-	/**
-	 * Returns a list which contains all occurrences of the selected identifier.
-	 * Checks if the selection is an identifier and if so if it is part of a
-	 * local variable.
-	 * 
-	 * @return All Occurrences in the Method of the selected identifier,
-	 *         immutable EmptyList if the above checks fail.
-	 */
-	private Collection<Identifier> selectedIdentifiersIfLocal() {
+	@Override
+	protected RefactoringStatus initializeRefactoring(IProgressMonitor pm){
+		RefactoringStatus ret=new RefactoringStatus();
 		// Find currently selected Element
 		Identifier currentlySelected = getSelectedIdentifier();
-		if (currentlySelected == null) { // The Selection is not an Identifier
-			return Collections.emptyList();
-		} else if (!astUtil4Variables.isLocalVariable(currentlySelected)) {
-			return Collections.emptyList();
-		}
 
 		// Find the CompoundStatement which declares the identifier
-		CompoundStatement declaringCompound = astUtil4Variables.findDeclaringCompoundStatement(currentlySelected);
-		Collection<Identifier> identifiers =astUtil4Variables.getAllIdentifiersWithoutOwnDeclaration(
-				declaringCompound, currentlySelected.getName());
-		return identifiers;
+		declaringCompound = astUtil4Variables.findDeclaringCompoundStatement(currentlySelected);
+		affectedIdentifiers =astUtil4Variables.getAllIdentifiersWithoutOwnDeclaration(declaringCompound, currentlySelected.getName());
+		containingFile=ActionHandlerUtil.getInputFile(info.getEditor());
+		if(containingFile==null){
+			ret.addFatalError("Couldnt find the file which contains the selection.");
+		}
+
+		return ret;
+		
 	}
 
 }
