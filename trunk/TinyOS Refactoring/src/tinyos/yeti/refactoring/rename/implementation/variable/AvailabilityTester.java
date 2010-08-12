31e15e52be726c7e80f7071516bfdfe634ab0de0
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-08-12 15:46:08 +0000

    Forgot to erase class before doing commit: Merged classes since a normal use case needs both of them.

--- trunk/TinyOS Refactoring/src/tinyos/yeti/refactoring/rename/implementation/variable/AvailabilityTester.java
+++ trunk/TinyOS Refactoring/src/tinyos/yeti/refactoring/rename/implementation/variable/AvailabilityTester.java
@@ -2,16 +2,14 @@ package tinyos.yeti.refactoring.rename.implementation.variable;
 
 import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
 import tinyos.yeti.refactoring.rename.RenameAvailabilityTester;
-import tinyos.yeti.refactoring.selection.ImplementationLocalVariableSelectionIdentifier;
+import tinyos.yeti.refactoring.selection.VariableSelectionIdentifier;
 
 public class AvailabilityTester extends RenameAvailabilityTester{
 
 	@Override
 	protected boolean isSelectionAppropriate(Identifier selectedIdentifier) {
-		ImplementationLocalVariableSelectionIdentifier selectionIdentifier=new ImplementationLocalVariableSelectionIdentifier(selectedIdentifier);
+		VariableSelectionIdentifier selectionIdentifier=new VariableSelectionIdentifier(selectedIdentifier);
 		return selectionIdentifier.isImplementationLocalVariable();
-//		ASTUtil4Variables asttil4Variables=new ASTUtil4Variables();
-//		return asttil4Variables.isImplementationLocalVariable(selectedIdentifier);
 	}
 	
 }
