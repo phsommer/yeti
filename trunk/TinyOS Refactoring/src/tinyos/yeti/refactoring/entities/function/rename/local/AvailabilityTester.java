c654fda5dfe1c06f207ce398ec1bfcaaf8c89ccb
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-08-13 13:09:35 +0000

    Refactored package structure to reach a even more intuitive structure.

---
+++ trunk/TinyOS Refactoring/src/tinyos/yeti/refactoring/entities/function/rename/local/AvailabilityTester.java
@@ -0,0 +1,17 @@
+package tinyos.yeti.refactoring.entities.function.rename.local;
+
+import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
+import tinyos.yeti.refactoring.entities.function.rename.global.FunctionSelectionIdentifier;
+import tinyos.yeti.refactoring.rename.RenameAvailabilityTester;
+
+public class AvailabilityTester extends RenameAvailabilityTester{
+
+	@Override
+	protected boolean isSelectionAppropriate(Identifier selectedIdentifier) {
+		FunctionSelectionIdentifier selectionIdentifier=new FunctionSelectionIdentifier(selectedIdentifier);
+		return selectionIdentifier.isImplementationLocalFunction();
+//		ASTUtil4Functions astUtil4Functions=new ASTUtil4Functions();
+//		return astUtil4Functions.isLocalFunction(selectedIdentifier);
+	}
+	
+}
