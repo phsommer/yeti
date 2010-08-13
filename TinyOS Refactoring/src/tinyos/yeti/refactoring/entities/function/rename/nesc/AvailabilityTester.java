080876b90c5e4506c8ab75eab3514345f4e65af6
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-08-13 13:15:15 +0000

    Refactored package structure to reach a even more intuitive structure.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/entities/function/rename/nesc/AvailabilityTester.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/entities/function/rename/nesc/AvailabilityTester.java
@@ -1,4 +1,4 @@
-package tinyos.yeti.refactoring.rename.function.nesc;
+package tinyos.yeti.refactoring.entities.function.rename.nesc;
 
 import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
 import tinyos.yeti.refactoring.rename.RenameAvailabilityTester;
