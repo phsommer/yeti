68b273e99d04a66a63bbdcafb32f3f84f3464f14
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-08-31 15:33:16 +0000

    Findes now some more ways of edited Variables

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/ast/StatementListAnalyzer.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/ast/StatementListAnalyzer.java
@@ -1,16 +1,20 @@
 package tinyos.yeti.refactoring.ast;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.Stack;
 
+import tinyos.yeti.nesc12.parser.ast.nodes.ASTNode;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.AssignmentExpression;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.Expression;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.IdentifierExpression;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.PostfixExpression;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.PrefixExpression;
+import tinyos.yeti.nesc12.parser.ast.nodes.expression.UnaryOperator;
 import tinyos.yeti.nesc12.parser.ast.nodes.expression.UnaryOperator.Operator;
 import tinyos.yeti.nesc12.parser.ast.nodes.general.Identifier;
 import tinyos.yeti.nesc12.parser.ast.nodes.statement.Statement;
@@ -22,31 +26,31 @@ public class StatementListAnalyzer extends AstAnalyzer {
 	private RefactoringInfo info;
 	private List<Statement> statements;
 	protected ASTUtil4Variables varUtil;
-	
-	public StatementListAnalyzer(List<Statement> statements, RefactoringInfo info){
+
+	public StatementListAnalyzer(List<Statement> statements,
+			RefactoringInfo info) {
 		super();
 		this.info = info;
 		this.statements = new LinkedList<Statement>(statements);
 		varUtil = new ASTUtil4Variables(getASTUtil());
 	}
-	
-	public Set<VariableDeclaration> getTopLevelDeclarations() {
-		return VariableDeclaration.getTopLevelDeclarations(statements,getInfo());
-	}
-	
 
-	
-	protected RefactoringInfo getInfo(){
+	public Set<VariableDeclaration> getTopLevelDeclarations() {
+		return VariableDeclaration.getTopLevelDeclarations(statements,
+				getInfo());
+	}
+
+	protected RefactoringInfo getInfo() {
 		return info;
 	}
-	
+
 	/**
 	 * Fields that are defined within the Statements to extract, but are still
 	 * visible after the End of the selection (so highest level declarations)
 	 * are not filterd out.
 	 */
 	public Set<String> getPotentionalyChangedLocalVariables() {
-		
+
 		Filter<Identifier> filter = new Filter<Identifier>() {
 			@Override
 			public boolean test(Identifier id) {
@@ -55,11 +59,46 @@ public class StatementListAnalyzer extends AstAnalyzer {
 			}
 		};
 
-		Set<Identifier> ids = varUtil.exploreVariableNamespace(statements, filter,info);
+		Set<Identifier> ids = varUtil.exploreVariableNamespace(statements,
+				filter, info);
 		Set<String> names = varUtil.getNames(ids);
 		return names;
 	}
-	
+
+	private boolean hasChildOfType(ASTNode node, Class<? extends ASTNode> type) {
+		LinkedList<Class<? extends ASTNode>> types = new LinkedList<Class<? extends ASTNode>>();
+		types.add(type);
+		return hasChildOfType(node, types);
+	}
+
+	private boolean hasChildOfType(ASTNode node,
+			Collection<Class<? extends ASTNode>> types) {
+		Stack<ASTNode> stack = new Stack<ASTNode>();
+		stack.push(node);
+		while (!stack.isEmpty()) {
+			node = stack.pop();
+			if (types.contains(node.getClass())) {
+				return true;
+			}
+			stack.addAll(astUtil.getChilds(node));
+		}
+		return false;
+	}
+
+	private boolean isChildOf(ASTNode parent, ASTNode child) {
+		Stack<ASTNode> stack = new Stack<ASTNode>();
+		ASTNode node;
+		stack.push(parent);
+		while (!stack.isEmpty()) {
+			node = stack.pop();
+			if (node.equals(child)) {
+				return true;
+			}
+			stack.addAll(astUtil.getChilds(node));
+		}
+		return false;
+	}
+
 	private boolean isModifing(Identifier id) {
 		boolean ret = false;
 
@@ -78,14 +117,18 @@ public class StatementListAnalyzer extends AstAnalyzer {
 		// TODO seems to have problems with brakes
 		PostfixExpression pfe = (PostfixExpression) astUtil.getParentForName(
 				id, PostfixExpression.class);
-		if (pfe != null) {
-			if (pfe.getExpression() instanceof IdentifierExpression) {
-				if (((IdentifierExpression) pfe.getExpression())
-						.getIdentifier().equals(id)) {
-					// The only Postfix Operators are Increment and Decrement
-					ret = true;
-				}
-			}
+		System.out.println("Postfix Operator info: "+(hasChildOfType(pfe.getExpression(),
+						IdentifierExpression.class)?"idExp,":"")+ ((!hasChildOfType(pfe.getExpression(), UnaryOperator.class))?"noUnary,":"")+
+				 (isChildOf(pfe.getExpression(), id)?"isId":""));
+		if (pfe != null
+				&& hasChildOfType(pfe.getExpression(),
+						IdentifierExpression.class)
+				&& (!hasChildOfType(pfe.getExpression(), UnaryOperator.class))
+				&& isChildOf(pfe.getExpression(), id)) {
+
+			// The only Postfix Operators are Increment and Decrement
+			ret = true;
+
 		}
 
 		// Prefix Operator ++/--/& (& makes it horriblely complex. I just
@@ -108,7 +151,7 @@ public class StatementListAnalyzer extends AstAnalyzer {
 
 		return ret;
 	}
-	
+
 	public Set<String> getInternalyUnusedDeclarations() {
 		Set<String> ret = new HashSet<String>();
 
@@ -124,7 +167,7 @@ public class StatementListAnalyzer extends AstAnalyzer {
 								return (new ASTUtil4Variables())
 										.isLocalVariableOrFunctionParameter(toTest);
 							}
-						},info);
+						}, info);
 				if (!varUtil.getNames(ids).contains(var)) {
 					ret.add(var);
 				}
