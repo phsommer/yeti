714aa7e6a26aab6ff20ee33b47e646422919f769
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-09-01 09:01:46 +0000

    Removed unused Code.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/entities/function/rename/global/GlobalFunctionAvailabilityTester.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/entities/function/rename/global/GlobalFunctionAvailabilityTester.java
@@ -10,8 +10,6 @@ public class GlobalFunctionAvailabilityTester extends RenameAvailabilityTester{
 	protected boolean isSelectionAppropriate(Identifier selectedIdentifier) {
 		FunctionSelectionIdentifier selectionIdentifier=new FunctionSelectionIdentifier(selectedIdentifier);
 		return selectionIdentifier.isGlobalFunction();
-//		ASTUtil4Functions astUtil4Functions=new ASTUtil4Functions();
-//		return astUtil4Functions.isGlobalFunction(selectedIdentifier);
 	}
 	
 }
