3b763ee1173ae484c016a40329865f8ca3427c76
urechm <urechm@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-09-02 11:10:48 +0000

    Added comment to the test function.

--- TinyOS Refactoring/src/tinyos/yeti/refactoring/IRefactoringAvailabilityTester.java
+++ TinyOS Refactoring/src/tinyos/yeti/refactoring/IRefactoringAvailabilityTester.java
@@ -3,5 +3,10 @@ package tinyos.yeti.refactoring;
 import org.eclipse.jface.text.ITextSelection;
 
 public interface IRefactoringAvailabilityTester{
+	/**
+	 * Checks if the given selection allows the refactoring to be executed.
+	 * @param selection
+	 * @return	true if the refactoring can be executed with the given selection, false otherwise.
+	 */
 	public boolean test(ITextSelection selection);
 }
