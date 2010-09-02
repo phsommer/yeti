ba578617639c6f5f33aa8e1838c55073d5ae1503
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-09-02 15:29:12 +0000

    Image explaining menustructure

--- trunk/TinyOS Refactoring/doc/make.bash
+++ trunk/TinyOS Refactoring/doc/make.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+dia --export=menuStructure.png -t png menuStructure.dia
 pdflatex doc.tex
 makeindex doc.nlo -s /usr/share/texmf-texlive/makeindex/nomencl/nomencl.ist -o doc.nls
 pdflatex doc.tex
