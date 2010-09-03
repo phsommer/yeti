ee3a9d8545894c3907c8a07bbaa5f7c98cca7ec9
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-09-03 12:08:59 +0000

    AstAnalyzerTypesHierarchy.dia gets now converted to a png

--- trunk/TinyOS Refactoring/doc/make.bash
+++ trunk/TinyOS Refactoring/doc/make.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 dia --export=menuStructure.png -t png menuStructure.dia
+dia --export=AstAnalyzerTypesHierarchy.png -t png AstAnalyzerTypesHierarchy.dia
 pdflatex doc.tex
 makeindex doc.nlo -s /usr/share/texmf-texlive/makeindex/nomencl/nomencl.ist -o doc.nls
 pdflatex doc.tex
