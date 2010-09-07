204f4fee5e0d5967f15fec71a2d0d8e9aa1a0183
heussern <heussern@2a69fd7c-12de-fc47-e5bb-db751a41839b>
2010-09-07 08:49:01 +0000

    new Class Diagramm for Changes

--- trunk/TinyOS Refactoring/doc/make.bash
+++ trunk/TinyOS Refactoring/doc/make.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
-dia --export=menuStructure.png -t png menuStructure.dia
-dia --export=AstAnalyzerTypesHierarchy.png -t png AstAnalyzerTypesHierarchy.dia
-dia --export=interactionDiagramRefactoring.png -t png interactionDiagramRefactoring.dia 
-dia --export=classDiagrammProcessorBasedRefactoring.png -t png classDiagrammProcessorBasedRefactoring.dia
+
+# Converting DIA-Files to PNG-Files
+for i in *.dia; do 
+    dia --export=${i%.dia}.png -t png "${i}"
+done
+
 pdflatex doc.tex
 makeindex doc.nlo -s /usr/share/texmf-texlive/makeindex/nomencl/nomencl.ist -o doc.nls
 pdflatex doc.tex
